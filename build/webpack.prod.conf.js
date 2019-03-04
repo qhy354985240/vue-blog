cfda1741e2e35988245f1ed8f25f3d1902978a44
qhy <qhyqinhuayon_i@didichuxing.com>
2019-03-04 11:35:38 +0800

    引入新的css打包插件 mincss

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -7,8 +7,9 @@ const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+// const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
 // const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 const env = process.env.NODE_ENV === 'testing'
@@ -56,14 +57,6 @@ const webpackConfig = merge(baseWebpackConfig, {
       'process.env': env
     }),
 
-    new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].[contenthash].css'),
-      // Setting the following option to `false` will not extract CSS from codesplit chunks.
-      // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
-      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`, 
-      // increasing file size: https://github.com/vuejs-templates/webpack/issues/1110
-      allChunks: true,
-    }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
     new OptimizeCSSPlugin({
@@ -100,7 +93,11 @@ const webpackConfig = merge(baseWebpackConfig, {
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
+    ]),
+
+    new MiniCssExtractPlugin({
+      filename: utils.assetsPath('[name]/css/[name].[contenthash].css')
+    })
   ]
 })
 
